c145d8439e2de6d1127f0cc7350977aa2d50b955
alextcup <atcupkovic@mun.ca>
2026-09-12 14:48:48 -0230

    Added Greeks + minor testing changes

---
+++ pricing_methods/greeks.py
@@ -0,0 +1,53 @@
+from dataclasses import replace
+
+from option import Option
+
+
+def delta(pricer, option: Option, h=1e-2, **kwargs):
+    dS = h * option.S0
+
+    up = replace(option, S0=option.S0 + dS)
+    down = replace(option, S0=option.S0 - dS)
+
+    return (pricer(up, **kwargs) - pricer(down, **kwargs)) / (2 * dS)
+
+
+def gamma(pricer, option: Option, h=5e-2, **kwargs):
+    dS = h * option.S0
+
+    up = replace(option, S0=option.S0 + dS)
+    down = replace(option, S0=option.S0 - dS)
+
+    return (pricer(up, **kwargs) - 2 * pricer(option, **kwargs) + pricer(down, **kwargs)) / dS ** 2
+
+
+def vega(pricer, option: Option, h=1e-2, **kwargs):
+    up = replace(option, sigma=option.sigma + h)
+    down = replace(option, sigma=option.sigma - h)
+
+    return (pricer(up, **kwargs) - pricer(down, **kwargs)) / (2 * h)
+
+
+def rho(pricer, option: Option, h=1e-2, **kwargs):
+    up = replace(option, r=option.r + h)
+    down = replace(option, r=option.r - h)
+
+    return (pricer(up, **kwargs) - pricer(down, **kwargs)) / (2 * h)
+
+
+def theta(pricer, option: Option, h=1e-2, **kwargs):
+    up = replace(option, T=option.T + h)
+    down = replace(option, T=option.T - h)
+
+    return -(pricer(up, **kwargs) - pricer(down, **kwargs)) / (2 * h)
+
+
+def all_greeks(pricer, option: Option, **kwargs):
+    price = pricer(option, **kwargs)
+    delta = delta(pricer, option, **kwargs)
+    gamma = gamma(pricer, option, **kwargs)
+    vega = vega(pricer, option, **kwargs)
+    theta = theta(pricer, option, **kwargs)
+    rho = rho(pricer, option, **kwargs)
+
+    return (price, delta, gamma, vega, theta, rho)
